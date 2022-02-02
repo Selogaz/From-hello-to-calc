c47b79991d4f1513cee81b9207d1b032fa3a3f03
Selogaz <terentjev.selogaz@yandex.ru>
2022-02-02 19:45:10 +0300

    extract 2 numbers

--- src/main/Start.java
+++ src/main/Start.java
@@ -6,24 +6,28 @@ public class Start {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         System.out.println("Input:");
-        int first_num = in.nextInt();
-        /*String s = in.nextLine();
-        //int char_max = s.length() - 1;
+        String s = in.nextLine();
+        String numberOnly = s.replaceAll("[^0-9]", "");
+        char[] number_massive = numberOnly.toCharArray();
+        int first_num = number_massive[0];
+        int second_num = number_massive[1];
+        int char_max = s.length() - 1;
         char[] ch = s.toCharArray();
-            for (int i = 0; i < char_max; i++) {
+          /*  for (int i = 0; i < char_max; i++) {
                 if (ch[i] == "+") {
                     result = first_num + second_num;
-                }
+                } else
                 if (ch[i] == "-") {
                     result = first_num - second_num;
-                }
+                } else
                 if (ch[i] == "/") {
                     result = first_num / second_num;
-                }
+                } else
                 if (ch[i] == "*" {
                     result = first_num * second_num;
                 }
             }*/
-        System.out.println(first_num);
+        System.out.println(number_massive[0]);
+        System.out.println(number_massive[1]);
     }
 }
