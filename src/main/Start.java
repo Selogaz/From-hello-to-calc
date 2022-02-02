3cdff9331e431bed426b49ab27b246fd2d471637
Selogaz <terentjev.selogaz@yandex.ru>
2022-02-02 19:32:39 +0300

    add massive

--- src/main/Start.java
+++ src/main/Start.java
@@ -6,7 +6,24 @@ public class Start {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         System.out.println("Input:");
-        String s = in.nextLine();
-        System.out.println(s);
+        int first_num = in.nextInt();
+        /*String s = in.nextLine();
+        //int char_max = s.length() - 1;
+        char[] ch = s.toCharArray();
+            for (int i = 0; i < char_max; i++) {
+                if (ch[i] == "+") {
+                    result = first_num + second_num;
+                }
+                if (ch[i] == "-") {
+                    result = first_num - second_num;
+                }
+                if (ch[i] == "/") {
+                    result = first_num / second_num;
+                }
+                if (ch[i] == "*" {
+                    result = first_num * second_num;
+                }
+            }*/
+        System.out.println(first_num);
     }
 }
