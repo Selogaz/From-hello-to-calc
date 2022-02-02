a8955b11dbb7016d863fd12bcd339e638dd2433f
Selogaz <terentjev.selogaz@yandex.ru>
2022-02-02 18:48:24 +0300

    adding 1 line

--- src/main/Start.java
+++ src/main/Start.java
@@ -1,4 +1,12 @@
 package main;
 
+import java.util.Scanner;
+
 public class Start {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input:");
+        String s = in.nextLine();
+        System.out.println(s);
+    }
 }
