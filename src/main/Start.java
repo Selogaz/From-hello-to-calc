c2c4587fb2b1ce31be1d5cbe697723117c57a3c1
Selogaz <terentjev.selogaz@yandex.ru>
2022-02-03 13:53:07 +0300

    don't works with num > 10

--- src/main/Start.java
+++ src/main/Start.java
@@ -10,14 +10,12 @@ public class Start {
         String s = in.nextLine();
         String numberOnly = s.replaceAll("[^0-9]", "");
         char[] number_massive = numberOnly.toCharArray();
-        int first_num;
-        int second_num;
         int numArr[] = new int[number_massive.length];
         for (int i = 0; i < number_massive.length; i++) {
             numArr[i] = number_massive[i];
         }
-        first_num = Character.getNumericValue(numArr[0]);
-        second_num = Character.getNumericValue(numArr[1]);
+        int first_num = Character.getNumericValue(numArr[0]);
+        int second_num = Character.getNumericValue(numArr[1]);
         if ((number_massive.length == 3) && (Character.getNumericValue(numArr[1]) == 0)){
             first_num = 10;
             second_num = Character.getNumericValue(numArr[2]);
@@ -27,6 +25,12 @@ public class Start {
         } else if ((number_massive.length == 4) && (Character.getNumericValue(numArr[3]) == 0)){
             first_num = 10;
             second_num = 10;
+        } else if ((number_massive.length == 4) && (((Character.getNumericValue(numArr[0] + numArr[1]) > 1) || (Character.getNumericValue(numArr[2] + numArr[3]) > 1)))) {
+          System.out.println("Не вводите числа больше 10!");
+          System.exit(0);
+        } else if ((number_massive.length == 3) && (((Character.getNumericValue(numArr[0] + numArr[1]) >= 2) || (Character.getNumericValue(numArr[1] + numArr[2]) >= 2)))) {
+            System.out.println("Не вводите числа больше 10!");
+            System.exit(0);
         }
         char[] ch = s.toCharArray();
         int result = 0;
@@ -44,6 +48,7 @@ public class Start {
                     result = first_num / second_num;
                 }
             }
+        System.out.println("Output:");
         System.out.println(result);
     }
 }
