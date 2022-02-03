fc18cff546d338498c31e36e53051dca07c51617
Selogaz <terentjev.selogaz@yandex.ru>
2022-02-03 13:18:45 +0300

    works with 10

--- src/main/Start.java
+++ src/main/Start.java
@@ -10,14 +10,24 @@ public class Start {
         String s = in.nextLine();
         String numberOnly = s.replaceAll("[^0-9]", "");
         char[] number_massive = numberOnly.toCharArray();
-        int first_num = 0;
-        int second_num = 0;
+        int first_num;
+        int second_num;
         int numArr[] = new int[number_massive.length];
         for (int i = 0; i < number_massive.length; i++) {
             numArr[i] = number_massive[i];
         }
         first_num = Character.getNumericValue(numArr[0]);
         second_num = Character.getNumericValue(numArr[1]);
+        if ((number_massive.length == 3) && (Character.getNumericValue(numArr[1]) == 0)){
+            first_num = 10;
+            second_num = Character.getNumericValue(numArr[2]);
+        } else if ((number_massive.length == 3) && (Character.getNumericValue(numArr[2]) == 0)){
+            second_num = 10;
+
+        } else if ((number_massive.length == 4) && (Character.getNumericValue(numArr[3]) == 0)){
+            first_num = 10;
+            second_num = 10;
+        }
         char[] ch = s.toCharArray();
         int result = 0;
            for (int i = 0; i < ch.length; i++) {
